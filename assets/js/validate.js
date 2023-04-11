c098821d87b6a31a72cfbd6118d712bee6c8f874
selvaG2 <selvaganapathikg2@gmail.com>
2023-04-11 08:05:06 +0530

    90%

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,36 +1,38 @@
 $(document).ready(function () {
-  $("#myform").validate({
+  $("#myForm").validate({
     rules: {
       username: {
         required: true,
-        pattern: /^(\w){5,8}$/,
+        pattern: /^(\w){5,8}$/
       },
       email: {
         required: true,
-        pattern: /^[\w]+@cgvakindia.com$/
+        pattern: /^[\w]+@cgvakindia\.com$/
       },
       phone: {
         required: true,
         pattern: /^(\d){10}$/
       },
-      comment:{
+      comment: {
         required: true
       }
     },
-    username: {
-        required: "This field cant be empty",
-        pattern: "It should contain only alphabet number and underscore",
+    messages: {
+      username: {
+        required: "This field can't be empty",
+        pattern: "It should contain only alphabet number and underscore"
       },
       email: {
-        required: "This field cant be empty",
-        pattern: "It should be cgvak email"
+        required: "This field can't be empty",
+        pattern: "It should be a CG-VAK email"
       },
       phone: {
-        required:"This field cant be empty",
-        pattern: "must contain 10 numbers only"
+        required: "This field can't be empty",
+        pattern: "Must contain 10 numbers only"
       },
-      comment:{
-        required: "This field cant be empty",
-    },
+      comment: {
+        required: "This field can't be empty"
+      }
+    }
   });
 });
